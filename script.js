7f05c41f1603af9f23ea0e5205173f2a0eb6a5cc
Jenn <hashbrowns120@gmail.com>
2021-07-28 12:24:33 -0500

    added DOM elements

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// TODO: add code here
+window.addEventListener('load', function(){
+    this.fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response) {
+        response.json().then(function(json) {
+            const container = document.getElementById('container');
+
+            for (prop in json) {
+                let newDiv = document.createElement('div');
+                newDiv.classList.add('astronaut'); 
+                newDiv.innerHTML = `
+                        <div class = 'bio'>
+                            <h3>${json[prop].firstName} ${json[prop].lastName}</h3>
+                            <ul>
+                                <li>Hours in Space: ${json[prop].hoursInSpace}</li>
+                                <li>Active: ${json[prop].active}</li>
+                                <li>Skills: ${json[prop].skills}</li>
+                            </ul>
+                        </div>
+                        <img class = 'avatar' src = '${json[prop].picture}'>
+                `
+                container.appendChild(newDiv);
+            }
+        });
+    });
+});
